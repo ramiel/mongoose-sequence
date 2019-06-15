36d825e9496502dd291b7afd5a1933ee5f40c712
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2019-06-15 10:40:35 +0200

    Installed eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
+};
